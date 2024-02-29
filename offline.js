9a3769367b0b09d3eb8fe9de497e1f8c54dcf037
OGGYBOY09 <161663290+OGGYBOY09@users.noreply.github.com>
2024-02-29 09:04:29 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1709166854,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/frame2cremovebgpreview-sheet0.png",
+		"images/bawah-sheet0.png",
+		"images/atas-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
